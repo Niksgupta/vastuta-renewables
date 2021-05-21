e8d37e3df207dca05e925d43e49949a9314ac852
nikhil <nikhilatoliya12@gmail.com>
2021-05-21 23:03:04 +0530

    first commit

---
+++ assets/js/success.js
@@ -0,0 +1,8 @@
+function success(){
+	
+     //     document.getElementById('name').value='';
+     //   document.getElementById('phone').value='';
+    	// document.getElementById('email').value='';
+        
+     //    document.getElementById('message').value='';
+}
